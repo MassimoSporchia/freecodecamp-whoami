6c1d427f7eae6c7d2a735be2ef52e507a99a3b31
Massimo Sporchia <massimosporchia@gmail.com>
2017-05-19 13:13:46 +0000

    finished example

---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/api/whoami',function(req,res){
+   var uagent = req.headers['user-agent']; 
+   var ip = req.headers['x-real-ip'] || req.connection.remoteAddress;
+   var locale = req.headers['accept-language'];
+   
+   res.send({ipaddress: ip, language: locale, software: uagent});
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port);
